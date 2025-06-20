716d1196fee2b51fbdf077f8a33c15e87d4d353b
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:05:08 +0000

    [automatic commit] 2025-06-20 05:05:08 UTC

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'cryptodata'
+    database: 'Dog'
 }).promise();
 
 /* GET users listing. */
