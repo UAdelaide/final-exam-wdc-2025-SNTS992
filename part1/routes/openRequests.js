d6488da577ad4700f1c8c9c179c750ad11eece1c
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:16:09 +0000

    [automatic commit] 2025-06-20 05:16:09 UTC

---
+++ part1/routes/openRequests.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql2'); // use mysql in application
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'DogWalkService'
+}).promise();
+
+/* Get list of dogs with their size and owner's username */
+router.get('/', async (req, res) => {
+    try {
+        const[dogs] = await db.query(`
+            SELECT d.dog_id, d.name, d.size, u.username As owner_username
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.user_id
+        `);
+        res.json(dogs);
+    } catch(err){
+        res.status(500).json({ error: err.message });
+    }
+});
+
+module.exports = router;
