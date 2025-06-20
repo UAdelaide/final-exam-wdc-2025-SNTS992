53d9ed1d1f802431078a0530d245e5554aa7b477
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:31:16 +0000

    [automatic commit] 2025-06-20 05:31:16 UTC

--- part1/routes/walkersSummary.js
+++ part1/routes/walkersSummary.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
             SELECT u.username AS walker, COUNT(wr.rating_id) AS total_rating,
                 AVG(wr.rating) AS average_rating
             FROM Users u
-            LEFT JOIN WalkRatings wr ON  u ON d.owner_id = u.user_id
+            LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
         `);
         res.json(summary);
     } catch(err){
