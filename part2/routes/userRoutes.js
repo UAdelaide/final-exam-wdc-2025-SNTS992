478ac1f95abfff0ba174e42378dd3a8bfca98e53
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 13:13:09 +0000

    [automatic commit] 2025-06-20 13:13:09 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,7 +88,7 @@ router.post('/logout', (res, req) => {
       console.error('Logout Error:', err);
       return res.status(500).json({ message: "Logout Failed" });
     }
-    res.clearCookie
+    res.json
   });
 });
 module.exports = router;
