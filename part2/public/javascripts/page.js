318b6ce8f3f969bad7e730d7b42e6dbf1a45d9c2
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 12:35:27 +0000

    [automatic commit] 2025-06-20 12:35:27 UTC

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -205,7 +205,6 @@ function login(event) {
     };
 
     // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
     xmlhttp.open("POST", "/api/users/login", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
