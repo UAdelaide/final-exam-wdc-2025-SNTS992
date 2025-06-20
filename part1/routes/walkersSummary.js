71343329cc5c464d8fd4ac9455650fa962f70b2e
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:44:15 +0000

    [automatic commit] 2025-06-20 05:44:15 UTC

--- part1/routes/walkersSummary.js
+++ part1/routes/walkersSummary.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
                 COUNT(completed_wr.request_id) AS completed_walks
             FROM Users u
             LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
-            LEFT JOIN WalkApplications wa ON wa.request_id = wa.walker_id
+            LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
             LEFT JOIN WalkRequests wrq ON wa.request_id = wrq.request_id AND wrq.status = 'completed'
             WHERE u.role = 'walker'
             GROUP BY u.user_id
