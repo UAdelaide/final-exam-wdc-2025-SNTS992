dfb0dba13a8dd64470936ab0c741890171b721ed
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 04:32:16 +0000

    [automatic commit] 2025-06-20 04:32:16 UTC

--- part1/app.js
+++ part1/app.js
@@ -97,10 +97,13 @@ let db;
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
         if (rows[0].count === 0) {
             await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users (username, email, password_hash, role)
+VALUES
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('johnysm', 'johnsmith@example.com', 'hashed666', 'walker'),
+('kevpat', 'kevinpatel@example.com', 'hashed888', 'owner');
       `);
         }
     } catch (err) {
