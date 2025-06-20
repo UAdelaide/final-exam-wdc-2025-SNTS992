e641973c4b8b5c8b255a5b60189ed82660d41e02
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:23:01 +0000

    [automatic commit] 2025-06-20 05:23:01 UTC

--- part1/routes/openRequests.js
+++ part1/routes/openRequests.js
@@ -14,8 +14,7 @@ router.get('/', async (req, res) => {
         const[open] = await db.query(`
             SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
-            JOIN Users u ON d.owner_id = u.user_id, 
-            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.owner_id = u.user_id, Dogs d ON wr.dog_id = d.dog_id
             WHERE wr.status = 'open'
         `);
         res.json(open);
