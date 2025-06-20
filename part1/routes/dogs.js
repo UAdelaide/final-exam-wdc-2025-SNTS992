9a1d60ee52c659909643a22259296f07553f8698
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:05:50 +0000

    [automatic commit] 2025-06-20 05:05:50 UTC

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql2'); // use mysql in application
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    user: 'root',
+    password: '',
     database: 'DogWalkService'
 }).promise();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
