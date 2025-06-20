e4e146454bd9a997157c1f690afc528857275e63
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 12:01:55 +0000

    [automatic commit] 2025-06-20 12:01:55 UTC

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const mysql = require('mysql2'); // use mysql in application
-const db = mysql.createPool({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
