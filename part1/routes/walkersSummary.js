9cb1fc47e3c80ed55a02ee7c7dcc5fe6ca121161
SNTS992 <unknown@student.adelaide.edu.au>
2025-06-20 05:38:29 +0000

    [automatic commit] 2025-06-20 05:38:29 UTC

--- part1/routes/walkersSummary.js
+++ part1/routes/walkersSummary.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
             SELECT u.username AS walker,
                 COUNT(wr.rating_id) AS total_rating,
                 AVG(wr.rating) AS average_rating
-                COUNT(DISTINCT )
+                COUNT(wa.request_id) AS 
             FROM Users u
             LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
             WHERE u.rolee = 'walker'
